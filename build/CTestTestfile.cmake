074ee2e831af785a563c401179e765b35ade2c44
Daniyal Ansari <daniyal.s.ansari@gmail.com>
2021-11-27 22:30:30 -0600

    removed build and devel

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/scuttle_ws/src
-# Build directory: /home/ros/scuttle_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("scuttle_description")
--- build/scuttle_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/scuttle_ws/src/scuttle_description
-# Build directory: /home/ros/scuttle_ws/build/scuttle_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

eb8c5ca79a8f959a7f1928b0f28af674eb36b947
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2021-11-29 16:06:18 -0600

    added rplidar

---
+++ master.sh
@@ -0,0 +1,5 @@
+# Configure Master ROS
+
+export ROS_MASTER_URI=http://localhost:11311
+export ROS_HOSTNAME=192.168.1.33
+export ROS_IP=192.168.1.33
---
+++ slave.sh
@@ -0,0 +1,5 @@
+# Configure Slave ROS
+
+export ROS_MASTER_URI=http://192.168.1.33:11311
+export ROS_HOSTNAME=192.168.1.26
+export ROS_IP=192.168.1.26

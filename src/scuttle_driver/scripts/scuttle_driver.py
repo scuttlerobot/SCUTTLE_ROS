85746d8629be0b2a61f0f77a55ed9721f820e7eb
Do-Kyoung Kwon <d-kwon@ti.com>
2022-04-25 15:11:48 -0500

    Updated scuttle_driver.py and scuttle_bringup.launch to enable/disable publishing of TF and joint state.

--- src/scuttle_driver/scripts/scuttle_driver.py
+++ src/scuttle_driver/scripts/scuttle_driver.py
@@ -28,15 +28,24 @@ def updatePose(msg):
 
 if __name__=="__main__":
 
-    scuttle = SCUTTLE(openLoop=True)
+    # Get the scuttle configuration file. Will get None if not specified
+    scuttle_config = rospy.get_param('/scuttle_driver/robot_config', None)
+
+    scuttle = SCUTTLE(config=scuttle_config, openLoop=True)
 
     rospy.init_node("scuttle_driver")
     r = rospy.Rate(50) # 50hz
 
+    # Get enable_tf_publish enable_joint_state_publish flags
+    enable_tf_publish          = rospy.get_param("/enable_tf_publish", True)
+    enable_joint_state_publish = rospy.get_param("/enable_joint_state_publish", True)
+
     rospy.Subscriber("/cmd_vel", Twist, commandVelocity)
     rospy.Subscriber("/initialpose", PoseWithCovarianceStamped, updatePose)
+
     odom_pub = rospy.Publisher("odom", Odometry, queue_size=50)
-    odom_broadcaster = tf.TransformBroadcaster()
+    if enable_tf_publish is True:
+        odom_broadcaster = tf.TransformBroadcaster()
 
     try:
 
@@ -50,53 +59,65 @@ if __name__=="__main__":
 
             odom_quat = tf.transformations.quaternion_from_euler(0, 0, th)
 
-            odom_broadcaster.sendTransform(
-            (x, y, 0.),
-            odom_quat,
-            current_time,
-            "base_link",
-            "odom"
-            )
+            # Publish TF when enable_tf_pulish is true
+            if enable_tf_publish is True:
+                odom_broadcaster.sendTransform(
+                (x, y, 0.),
+                odom_quat,
+                current_time,
+                "base_link",
+                "odom"
+                )
 
             odom = Odometry()
             odom.header.stamp = current_time
             odom.header.frame_id = "odom"
 
             odom.pose.pose = Pose(Point(x, y, 0.), Quaternion(*odom_quat))
+            # Since some ROS nodes expect non-zero pose covariance,
+            # set non-zero default pose covariance matrix.
+            odom.pose.covariance = [0.01, 0, 0, 0, 0, 0,
+                                    0, 0.01, 0, 0, 0, 0,
+                                    0, 0, 0.01, 0, 0, 0,
+                                    0, 0, 0, 0.1, 0, 0,
+                                    0, 0, 0, 0, 0.1, 0,
+                                    0, 0, 0, 0, 0, 0.1]
 
             odom.child_frame_id = "base_link"
             odom.twist.twist = Twist(Vector3(0, vx, 0), Vector3(0, 0, 0))
 
             odom_pub.publish(odom)
 
-            myJointStatePublisher = rospy.Publisher('joint_states', JointState, queue_size=10)
-            # pub = rospy.Publisher('joint_states', JointState, queue_size=10)
-            # rospy.init_node('joint_state_publisher')
-            jointState = JointState()
+            # Publish joint state when enable_joint_state_publish is true
+            if enable_joint_state_publish is True:
+                myJointStatePublisher = rospy.Publisher('joint_states', JointState, queue_size=10)
+                # pub = rospy.Publisher('joint_states', JointState, queue_size=10)
+                # rospy.init_node('joint_state_publisher')
+                jointState = JointState()
 
-            jointState.header = Header()
+                jointState.header = Header()
 
-            jointState.header.stamp = rospy.Time.now()
+                jointState.header.stamp = rospy.Time.now()
 
-            jointState.name = ['l_wheel_joint',
-                               'r_wheel_joint',
-                               'r_caster_swivel_joint',
-                               'l_caster_swivel_joint',
-                               'r_caster_wheel_joint',
-                               'l_caster_wheel_joint'
-                              ]
-
-            jointState.position = [scuttle.leftWheel.encoder.position * ((2 * np.pi) / scuttle.leftWheel.encoder.resolution),
-                                   scuttle.rightWheel.encoder.position * ((2 * np.pi) / scuttle.rightWheel.encoder.resolution),
-                                   0,
-                                   0,
-                                   0,
-                                   0
+                jointState.name = ['l_wheel_joint',
+                                   'r_wheel_joint',
+                                   'r_caster_swivel_joint',
+                                   'l_caster_swivel_joint',
+                                   'r_caster_wheel_joint',
+                                   'l_caster_wheel_joint'
                                   ]
 
-            jointState.velocity = []
-            jointState.effort = []
-            myJointStatePublisher.publish(jointState)
+                jointState.position = [scuttle.leftWheel.encoder.position * ((2 * np.pi) / scuttle.leftWheel.encoder.resolution),
+                                       scuttle.rightWheel.encoder.position * ((2 * np.pi) / scuttle.rightWheel.encoder.resolution),
+                                       0,
+                                       0,
+                                       0,
+                                       0
+                                      ]
+
+                jointState.velocity = []
+                jointState.effort = []
+                myJointStatePublisher.publish(jointState)
 
             r.sleep()
 
